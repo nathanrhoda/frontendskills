cc2c11738d87faa051c2bd1ae1ebc2d8434a9440
Nathan Rhoda <nathan.rhoda@hotmail.com>
2021-03-16 01:06:28 +0200

    Angular check

--- compare-angular/src/app/app.module.ts
+++ compare-angular/src/app/app.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { FaqComponent } from './faq/faq.component';
+
 
 @NgModule({
   declarations: [
@@ -14,7 +16,8 @@ import { FaqComponent } from './faq/faq.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- compare-angular/src/app/faq/faq.component.ts
+++ compare-angular/src/app/faq/faq.component.ts
@@ -1,13 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-faq',
   templateUrl: './faq.component.html',
   styleUrls: ['./faq.component.sass']
 })
+
 export class FaqComponent implements OnInit {
 
-  constructor() { }
+  faqs: Array<any>;
+
+  constructor(private http: HttpClient) {
+
+    this.http.get<any>('https://jsonplaceholder.typicode.com/posts')
+      .subscribe(data => {
+        this.faqs = data;
+      });
+
+  }
 
   ngOnInit(): void {
   }
